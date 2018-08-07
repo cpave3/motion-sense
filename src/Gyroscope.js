27797d40e88a3ff4116d2adb6d3bab1ed761f295
Cameron Pavey <cpave3@gmail.com>
2018-08-08 08:53:37 +1000

    :recycle: Updated Example, Added event handler
      - ADD: Lifecycle events
      - ADD: New features to example

--- src/Gyroscope.js
+++ src/Gyroscope.js
@@ -12,29 +12,56 @@ class Gyroscope extends CoreSensor {
             this.config[option] = options[option] || defaultOptions[option];
         });
         this.readyToEmit = true;
+        this.running = false;
+        this.internals();
+
     }
+
+    /**
+     * These closures will fire when their keyed event happens
+     */
+    internals() {
+        const events = {
+            started: event => {
+                this.running = true;
+            },
+            stopped: event => {
+                this.running = false;
+            }
+        };
+
+        Object.keys(events).forEach(eventName => {
+            this.addEventListener(eventName, events[eventName]);
+        });
+    };
 
     isReady() {
         return !!window.DeviceMotionEvent;
     };
 
     /**
-     * Start emitting data from the sensor
+     * This method handles the data coming from the DeviceMotionEvent
+     * @param {DeviceMotionEvent} event 
+     */
+    handleDeviceMotion(event) {
+        /**
+         * Each time we get data from the sensor, we need to check if it
+         * is time to emit a new event yet. This is based on the user-defined
+         * frequency passed to the constructor
+         */
+        if (this.readyToEmit) {
+            this.dispatchEvent(new CustomEvent('reading', {detail: {data: event.rotationRate}}));
+            this.readyToEmit = false;
+        }
+    };   
+
+    /**
+     * This method checks if the device is ready and starts reading data at the requested frequency
      */
     start() {
        if(this.isReady()) {
            this.initClock();
-           window.addEventListener('devicemotion', (event) => {
-               /**
-                * Each time we get data from the sensor, we need to check if it
-                * is time to emit a new event yet. This is based on the user-defined
-                * frequency passed to the constructor
-                */
-               if (this.readyToEmit) {
-                   this.dispatchEvent(new CustomEvent('read', {detail: {data: event.rotationRate}}));
-                   this.readyToEmit = false;
-               }
-           });            
+           window.addEventListener('devicemotion', event => this.handleDeviceMotion(event));         
            this.dispatchEvent(new Event('started'));
        } else {
             // We cannot access the device motion event, so this won't work
@@ -43,17 +70,29 @@ class Gyroscope extends CoreSensor {
        }
     };
 
+    /**
+     * This method removes the event listener and stops the flow of data from the sensor
+     */
     stop() {
-        window.removeEventListener('devicemotion');
+        window.removeEventListener('devicemotion', event => this.handleDeviceMotion(event));
         this.dispatchEvent(new Event('stopped'));
         clearInterval(this.intervalTicker);
     };
 
+    /**
+     * This method sets the desired number of sensor reads per second
+     * @param {number} newFrequency 
+     */
     setFrequency(newFrequency) {
         this.config.frequency = newFrequency;
-        this.initClock();
+        if (this.running) {
+            this.initClock();
+        }
     }
 
+    /**
+     * This method is used to start the internal timer upon which the sensor readings depend
+     */
     initClock() {
         const { frequency } = this.config;
         if (this.intervalTicker) {
@@ -61,7 +100,7 @@ class Gyroscope extends CoreSensor {
         }
         this.readyToEmit = false;
         // We should be able to get sensor data from the device
-        const interval = Math.round(1000 / frequency, 0);
+        const interval = Math.round(1000 / frequency);
         this.intervalTicker = setInterval(() => {
             this.readyToEmit = true;
         }, interval);
